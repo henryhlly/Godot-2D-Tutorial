0e53807dbf3076ec9b5f875278ed979b3e3ed6a6
Henry Ly <lyhenry2004@gmail.com>
2024-06-25 12:30:49 +1200

    Added Maze and camera to follow player

---
+++ SpeedySaucer/Player.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var force = 1000
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	var force = 1000
+	if Input.is_action_pressed("Up"):
+		apply_force(Vector2(0,-force))
+	if Input.is_action_pressed("Down"):
+		apply_force(Vector2(0,force))
+	if Input.is_action_pressed("Left"):
+		apply_force(Vector2(-force,0))
+	if Input.is_action_pressed("Right"):
+		apply_force(Vector2(force,0))
