abab9b0c9b0e78179a75d176de5c9ff6ff5bee6b
Henry Ly <lyhenry2004@gmail.com>
2024-07-01 13:34:37 +1200

    Completed the Alien Attack game

--- AlienAttack/scripts/enemy_spawner.gd
+++ AlienAttack/scripts/enemy_spawner.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 signal enemy_spawned(enemy_instance)
+signal path_enemy_spawned(path_enemy_instance)
+
 var enemy_scene = preload("res://scenes/enemy.tscn")
+var path_enemy_scene = preload("res://scenes/path_enemy.tscn")
 
 @onready var spawn_positions = $SpawnPositions
 
@@ -13,5 +16,12 @@ func spawn_enemy():
 	enemy_instance.global_position = random_spawn_position.global_position
 	emit_signal("enemy_spawned", enemy_instance)
 
+func spawn_path_enemy():
+	var path_enemy_instance = path_enemy_scene.instantiate()
+	emit_signal("path_enemy_spawned", path_enemy_instance)
+
 func _on_timer_timeout():
 	spawn_enemy()
+
+func _on_path_enemy_timer_timeout():
+	spawn_path_enemy()
