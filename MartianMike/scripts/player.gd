d20462bd138be351f5dc19e8e1dd5234edc0be07
Henry Ly <lyhenry2004@gmail.com>
2024-07-06 22:17:15 +1200

    Implemented two traps and jump pad

--- MartianMike/scripts/player.gd
+++ MartianMike/scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 @export var gravity = 400
 @export var speed = 125
@@ -13,9 +14,28 @@ func _physics_process(delta):
 		if velocity.y > 500:
 			velocity.y = 500
 	
-	if Input.is_action_just_pressed("Key_Jump"):
-		velocity.y += -jump_force
+	if Input.is_action_just_pressed("Key_Jump") && is_on_floor():
+		jump(jump_force)
 	
 	var direction = Input.get_axis("Key_Left", "Key_Right")
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
 	velocity.x = direction*speed
+	
 	move_and_slide()
+	update_animation(direction)
+
+func update_animation(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+
+func jump(force):
+	velocity.y = -force
