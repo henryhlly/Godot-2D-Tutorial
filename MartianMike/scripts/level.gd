94f64b457411d3ce406b2219df3e5e5d0d4a4831
Henry Ly <lyhenry2004@gmail.com>
2024-07-12 02:00:43 +1200

    Set a win condition and a level timer

--- MartianMike/scripts/level.gd
+++ MartianMike/scripts/level.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
 @export var next_level: PackedScene = null
+@export var level_time = 5
+@export var is_final_level: bool = false
 
 @onready var start = $Start
-@onready var player = null
 @onready var exit = $Exit
 @onready var deathzone = $Deathzone
+@onready var hud = $UILayer/HUD
+@onready var ui_layer = $UILayer
+
+var timer_node = null
+var player = null
+var time_left
+var win = false
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -20,6 +28,27 @@ func _ready():
 		
 	exit.body_entered.connect(_on_exit_body_entered)
 	deathzone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	hud.set_time_label(time_left)
+	
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timeout)
+	add_child(timer_node)
+	timer_node.start()
+	
+func _on_level_timeout():
+	if win == false:
+		time_left -= 1
+		hud.set_time_label(time_left)
+		
+		if time_left <= 0:
+			reset_player()
+			time_left = level_time
+			hud.set_time_label(time_left)
+	
 
 func _process(delta):
 	if Input.is_action_just_pressed("Key_Quit"):
@@ -36,12 +65,18 @@ func _on_trap_touched_player():
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start.get_spawn_pos()
+	time_left = level_time
+	hud.set_time_label(time_left)
 
 func _on_exit_body_entered(body):
 	if body is Player:
-		if next_level != null:
+		if is_final_level || (next_level != null):
+			win = true
 			exit.animate()
 			body.active = false
 			body.velocity = Vector2.ZERO
 			await get_tree().create_timer(1.5).timeout
-			get_tree().change_scene_to_packed(next_level)
+			if is_final_level:
+				ui_layer.show_win_screen(true)
+			else:
+				get_tree().change_scene_to_packed(next_level)
